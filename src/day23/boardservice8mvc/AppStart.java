a72828dc5dbbfa79e772a977d49cda570eb45677
YimJunsu <yimjunsu@gmail.com>
2024-11-17 10:33:59 +0900

    BoardService8mvc

--- src/day23/boardservice8mvc/AppStart.java
+++ src/day23/boardservice8mvc/AppStart.java
@@ -8,7 +8,14 @@ public class AppStart {
         // 메인 뷰 요청
         // - 다른 클래스의 존재하는 함수를 호출하는 방법
         // -- 1. 객체를 생성한 후 객체명.메소드명()
-        BoardView view = new BoardView();
-        view.mainPage();
+
+        /*싱글톤을 사용하기 전*/
+//        BoardView view = new BoardView();
+//        view.mainPage();
+
+        /*싱글톤을 사용했을 때*/
+        BoardView.getInstance().mainPage();
+            //BoardView.getInstance() ==> boardView
+            //boardView.mainPage();
     }
 }
